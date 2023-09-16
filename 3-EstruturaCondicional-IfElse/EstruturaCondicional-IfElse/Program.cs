4289fd56e3d9b57ab01c7021ea001a99a3e66884
Tarcisio <tarcisiosaf@gmail.com>
2023-09-16 12:07:47 -0300

    Organização de Pasas

---
+++ 3-EstruturaCondicional-IfElse/EstruturaCondicional-IfElse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program {
+    class Program {
+        static void Main(string[] args) {
+            int idade = 18;
+            bool possuiCNH = false;
+            bool podeDirigir;
+
+            if (idade >= 18 && possuiCNH) {
+                podeDirigir = true;
+                Console.WriteLine("Você pode dirigir.");
+            } else if (idade >= 18) {
+                podeDirigir = false;
+                Console.WriteLine("Você não pode dirigir pois não possui CNH.");
+            } else {
+                podeDirigir = false;
+                Console.WriteLine("Você não tem a idade permitida para dirigir.");
+            }
+        }
+    }
+}
