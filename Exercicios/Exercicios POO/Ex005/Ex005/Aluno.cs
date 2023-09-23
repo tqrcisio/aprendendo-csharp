08f4c55207e6998f2c7db28a803f33c102453a0a
Tarcisio <tarcisiosaf@gmail.com>
2023-09-22 21:30:30 -0300

    Exercicio 05 finalizado

---
+++ Exercicios/Exercicios POO/Ex005/Ex005/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Ex005 {
+    internal class Aluno {
+        public string Nome;
+        public double A1;
+        public double A2;
+        public double A3;
+
+        public double NotaFinal() {
+            return A1 + A2 + A3;
+        }
+        public string Resultado() {
+            string saida = "";
+            double diff = 60.00 - NotaFinal();
+            if (NotaFinal() >= 60.00) {
+                saida = "APROVADO";
+            } else {
+                saida = $"REPROVADO\nFALTARAM {diff} PONTOS";
+            }
+            return saida;
+        }
+        public override string ToString() {
+            return $"NOTA FINAL = {NotaFinal()}\n{Resultado()}";
+        }
+    }
+}
---
+++ Exercicios/Exercicios POO/Ex005/Ex005/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex005 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Nome do aluno: ");
+            Aluno aluno = new Aluno();
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno.A1 = double.Parse(Console.ReadLine());
+            aluno.A2 = double.Parse(Console.ReadLine());
+            aluno.A3 = double.Parse(Console.ReadLine());
+            Console.WriteLine(aluno);
+        }
+    }
+}
